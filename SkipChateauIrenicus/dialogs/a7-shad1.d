9affa44135aba1e15bbe6320b9565bfbc7452efe
Argent77 <argent77@server.fake>
2016-08-15 18:13:44 +0200

    Improved rogue dialog options and minor fixes in item table

--- SkipChateauIrenicus/dialogs/a7-shad1.d
+++ SkipChateauIrenicus/dialogs/a7-shad1.d
@@ -26,21 +26,30 @@ END
 
 IF ~~ thief.3
   SAY @2008 /* I don't see why not. You need a key to use them, though. Luckily for you I grabbed a few spare keys. You can have one if you want. */
-  ++ @2009 /* I'll take the key. Thanks. */ + thief.bye
+  + ~!PartyHasItem("MISC4G")~ + @2009 /* I'll take the key. Thanks. */ DO ~GiveItemCreate("MISC4G", LastTalkedToBy, 0, 0, 0) ActionOverride("Chest8", DestroyItem("MISC4G"))~ + thief.bye.1
+  + ~PartyHasItem("MISC4G")~ + @2014 /* No thanks. I've got one already. */ + thief.bye.2
   ++ @2010 /* No need. I'd rather find the way out by myself. */ + thief.rejected
 END
 
-IF ~~ thief.bye
-  SAY @2011 /* Here you go. Now if you'll excuse me. I've got to hunt down a treacherous mage. */
+IF ~~ thief.bye.1
+  SAY @2011 /* Here you go. */
+  IF ~~ SOLVED_JOURNAL @5001 + thief.bye.final
+END
+
+IF ~~ thief.bye.2
+  SAY @2015 /* Suit yourself. */
+  IF ~~ SOLVED_JOURNAL @5002 + thief.bye.final
+END
+
+IF ~~ thief.bye.final
+  SAY @2016 /* Now if you'll excuse me. I've got to hunt down a treacherous mage. */
   IF ~~ DO ~SetGlobal("A7-ShortcutEnabled", "GLOBAL", 1)
             SetGlobal("A7-Cutscene", "MYAREA", 2)
-            GiveItemCreate("MISC4G", LastTalkedToBy, 0, 0, 0)
-            ActionOverride("Chest8", DestroyItem("MISC4G"))
-            EscapeAreaObject("JailPortal")~ SOLVED_JOURNAL @5000 EXIT
+            EscapeAreaObject("JailPortal")~ EXIT
 END
 
 IF ~~ thief.rejected
   SAY @2012 /* I don't care anyway. I've got to hunt down a treacherous mage. */
   IF ~~ DO ~SetGlobal("A7-Cutscene", "MYAREA", 2)
-            EscapeArea()~ SOLVED_JOURNAL @5001 EXIT
+            EscapeArea()~ SOLVED_JOURNAL @5000 EXIT
 END
